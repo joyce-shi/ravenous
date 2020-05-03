0998f02ab153625247d2df428ffc59472909533c
Joyce Shi <jshi@fispan.com>
2020-05-03 18:00:11 -0400

    adding search bar functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../logo.svg';
+// import logo from '../../logo.svg';
 import BusinessList from '../BusinessList/BusinessList'
 import SearchBar from '../SearchBar/SearchBar'
 
@@ -26,14 +26,20 @@ const businesses = [
   business,
 ];
 
-function App() {
-  return (
-    <div class="App">
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList businesses={businesses}/>
-    </div>
-  );
+class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location} and ${sortBy}`);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp}/>
+        <BusinessList businesses={businesses}/>
+      </div>
+    );
+  }
 }
 
 export default App;
